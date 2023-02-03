3e93f49aecd732f0d8a46110fc0ea1957407b234
Dhruvil Dobariya <190540107051@darshan.ac.in>
2023-02-03 23:12:45 +0530

    Create Repository, Data and Entity Layer

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration) 
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString")); // connect with database
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName")); // This method get database, if database not exist then first it should create database
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
